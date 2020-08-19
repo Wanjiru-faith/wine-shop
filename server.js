06cac57266e9334cd90ae474669194c393374b07
Faith <Wanjiru-faith>
2020-08-19 10:52:19 +0300

    trial five

--- server.js
+++ server.js
@@ -1,8 +1,17 @@
+"use strict";
 const express = require('express')
 const serveStatic = require('serve-static')
 const path = require('path')
 
 const app = express()
+const allowCrossDomain = function(req, res, next) { 
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', '*');
+    res.header('Access-Control-Allow-Headers', '*');
+    next();
+}
+    // 
+app.use(allowCrossDomain)
 
 //here we are configuring dist to serve app files
 app.use(express.static(path.join(__dirname, '../public')));
